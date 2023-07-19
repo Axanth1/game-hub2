d8d7c993afa1a1d241da00fdb5b14f39e13ccc8d
Alexandros Xanthopoulos <xanthopoulosa22@gmail.com>
2023-07-20 00:09:19 +0300

    build expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,17 +1,18 @@
 import { Heading, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
-  if (isLoading) return <Spinner/>
+  if (isLoading) return <Spinner />;
   if (error || !game) throw error;
 
   return (
     <>
-        <Heading>{game.name}</Heading>
-        <Text>{game.description_raw}</Text>
+      <Heading>{game.name}</Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
